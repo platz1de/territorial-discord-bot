4fdae26e37b7534d8dfbac4ab5dce7469a3d5fb9
platz1de <51201131+platz1de@users.noreply.github.com>
2023-07-30 23:21:38 +0200

    Remove invalid data, send warning if permissions aren't set up properly

--- commands/SettingsCommand.ts
+++ commands/SettingsCommand.ts
@@ -1,6 +1,6 @@
 import {ChatInputCommandInteraction, Colors, EmbedBuilder, Message, NewsChannel, PermissionFlagsBits, SlashCommandBuilder, TextChannel} from "discord.js";
 import {setServerSetting} from "../BotSettingProvider";
-import {rewards} from "../PointManager";
+import {client, rewards} from "../PointManager";
 import {BotUserContext} from "../util/BotUserContext";
 
 const settingNames = ["setprefix", "toggleroles", "addchannel", "removechannel", "setlogchannel", "setupdatechannel", "addmodrole", "removemodrole", "addrewardrole", "removerewardrole"];
@@ -49,45 +49,96 @@ export default {
 
 async function showSettingsEmbed(context: BotUserContext) {
 	const prefix = context.context.prefix;
-	context.reply({
-		embeds: [
-			new EmbedBuilder().setAuthor({name: context.guild.name, iconURL: context.guild.iconURL() || undefined})
-				.setTitle("Server Settings")
-				.addFields([
-					{
-						name: "❗ Prefix",
-						value: `\`${prefix}\`\nUse \`${prefix}setprefix <prefix>\` to change`, inline: true
-					},
-					{
-						name: "🏷️ Roles",
-						value: `\`${context.context.roles}\` (${context.context.roles === "all" ? "Keep all roles a member has unlocked" : "Only the highest role a member unlocked"})\nUse \`${prefix}toggleroles\` to toggle`, inline: true
-					},
-					{
-						name: "👑 Win Channels",
-						value: context.context.channel_id.map((id) => `<#${id}>`).join("\n") + `\nUse \`${prefix}removechannel <id>\` or \`${prefix}addchannel <id>\` to manage`, inline: true
-					},
-					{
-						name: "📜 Log Channel",
-						value: `<#${context.context.log_channel_id}>\nUse \`${prefix}setlogchannel <id>\` to change`, inline: true
-					},
-					{
-						name: "📰 Update Channel",
-						value: `<#${context.context.update_channel_id}>\nUse \`${prefix}setupdatechannel <id>\` to change`, inline: true
-					},
-					{
-						name: "🛠 Mod Roles",
-						value: context.context.mod_roles.map((id) => `<@&${id}>`).join("\n") + `\nUse \`${prefix}removemodrole <id>\` or \`${prefix}addmodrole <id>\` to manage`, inline: true
-					},
-					{
-						name: "🏆 Reward Roles",
-						value: `See all currect reward roles using \`${prefix}roles\`\nUse \`${prefix}removerewardrole <id>\` or \`${prefix}addrewardrole <id> <points|wins> <amount>\` to manage`, inline: true
-					}
-				])
-				.setColor(Colors.Blurple).setFooter({
-				text: "All settings can also be changed using /settings",
-			}).setTimestamp().toJSON()
-		]
-	}).catch(console.error)
+	let changes = [];
+	for (const id of context.context.channel_id) {
+		const channel = context.guild.channels.cache.get(id);
+		if (!channel || !(channel instanceof TextChannel || channel instanceof NewsChannel)) {
+			changes.push(`Removed invalid channel ${id} from the win channels`);
+			context.context.channel_id.splice(context.context.channel_id.indexOf(id), 1);
+		} else {
+			if (channel.permissionsFor(client.user!)?.has(PermissionFlagsBits.ReadMessageHistory) !== true || channel.permissionsFor(client.user!)?.has(PermissionFlagsBits.ViewChannel) !== true) {
+				changes.push(`I don't have permission to read messages in ${channel}`);
+			}
+			if (channel.permissionsFor(client.user!)?.has(PermissionFlagsBits.SendMessages) !== true || channel.permissionsFor(client.user!)?.has(PermissionFlagsBits.ViewChannel) !== true) {
+				changes.push(`I don't have permission to send messages in <#${channel}`);
+			}
+		}
+	}
+	const logChannel = context.guild.channels.cache.get(context.context.log_channel_id);
+	if (!logChannel || !(logChannel instanceof TextChannel || logChannel instanceof NewsChannel)) {
+		changes.push(`Invalid log channel ${logChannel}`);
+	} else {
+		if (logChannel.permissionsFor(client.user!)?.has(PermissionFlagsBits.SendMessages) !== true || logChannel.permissionsFor(client.user!)?.has(PermissionFlagsBits.ViewChannel) !== true) {
+			changes.push(`I don't have permission to send messages in ${logChannel}`);
+		}
+	}
+	const updateChannel = context.guild.channels.cache.get(context.context.update_channel_id);
+	if (!updateChannel || !(updateChannel instanceof TextChannel || updateChannel instanceof NewsChannel)) {
+		changes.push(`Invalid update channel ${updateChannel}`);
+	} else {
+		if (updateChannel.permissionsFor(client.user!)?.has(PermissionFlagsBits.SendMessages) !== true || updateChannel.permissionsFor(client.user!)?.has(PermissionFlagsBits.ViewChannel) !== true) {
+			changes.push(`I don't have permission to send messages in ${updateChannel}`);
+		}
+	}
+	for (const role of context.context.mod_roles) {
+		if (!context.guild.roles.cache.has(role)) {
+			changes.push(`Removed invalid role ${role} from the mod roles`);
+			context.context.mod_roles.splice(context.context.mod_roles.indexOf(role), 1);
+		}
+	}
+	if (context.context.rewards.length !== 0) {
+		if (!context.guild.members.cache.get(client.user!.id)?.permissions.has(PermissionFlagsBits.ManageRoles)) {
+			changes.push("I don't have permission to manage roles");
+		}
+	}
+	for (const role of context.context.rewards) {
+		if (!context.guild.roles.cache.has(role.role_id)) {
+			changes.push(`Removed invalid role ${role.role_id} from the reward roles`);
+			context.context.rewards.splice(context.context.rewards.indexOf(role), 1);
+		} else if (context.guild.roles.cache.get(role.role_id)?.editable !== true) {
+			changes.push(`I don't have permission to give out <@&${role.role_id}>`);
+		}
+	}
+	let embeds = [];
+	if (changes.length > 0) {
+		embeds.push(new EmbedBuilder().setTitle("Warning").setDescription(changes.join("\n")).setColor(Colors.Yellow).toJSON());
+	}
+	embeds.push(new EmbedBuilder().setAuthor({name: context.guild.name, iconURL: context.guild.iconURL() || undefined})
+		.setTitle("Server Settings")
+		.addFields([
+			{
+				name: "❗ Prefix",
+				value: `\`${prefix}\`\nUse \`${prefix}setprefix <prefix>\` to change`, inline: true
+			},
+			{
+				name: "🏷️ Roles",
+				value: `\`${context.context.roles}\` (${context.context.roles === "all" ? "Keep all roles a member has unlocked" : "Only the highest role a member unlocked"})\nUse \`${prefix}toggleroles\` to toggle`, inline: true
+			},
+			{
+				name: "👑 Win Channels",
+				value: context.context.channel_id.map((id) => `<#${id}>`).join("\n") + `\nUse \`${prefix}removechannel <id>\` or \`${prefix}addchannel <id>\` to manage`, inline: true
+			},
+			{
+				name: "📜 Log Channel",
+				value: `<#${context.context.log_channel_id}>\nUse \`${prefix}setlogchannel <id>\` to change`, inline: true
+			},
+			{
+				name: "📰 Update Channel",
+				value: `<#${context.context.update_channel_id}>\nUse \`${prefix}setupdatechannel <id>\` to change`, inline: true
+			},
+			{
+				name: "🛠 Mod Roles",
+				value: context.context.mod_roles.map((id) => `<@&${id}>`).join("\n") + `\nUse \`${prefix}removemodrole <id>\` or \`${prefix}addmodrole <id>\` to manage`, inline: true
+			},
+			{
+				name: "🏆 Reward Roles",
+				value: `See all currect reward roles using \`${prefix}roles\`\nUse \`${prefix}removerewardrole <id>\` or \`${prefix}addrewardrole <id> <points|wins> <amount>\` to manage`, inline: true
+			}
+		])
+		.setColor(Colors.Blurple).setFooter({
+			text: "All settings can also be changed using /settings",
+		}).setTimestamp().toJSON());
+	context.reply({embeds}).catch(console.error)
 }
 
 async function handleSetting(context: BotUserContext, index: number) {
