21a34de2a977c62d91adbb448d2ce2eb45746eae
platz1de <51201131+platz1de@users.noreply.github.com>
2023-11-13 12:09:25 +0100

    Add TOS and privacy policy to about command

--- commands/AboutCommand.ts
+++ commands/AboutCommand.ts
@@ -21,6 +21,8 @@ async function showAboutEmbed(context: BotUserContext) {
 					{name: "Commands", value: `Use </help:1129906100985151527> to see all commands`},
 					{name: "Uptime", value: formatTime(Math.floor(process.uptime()))},
 					{name: "Source", value: "https://github.com/territorialHQ/point-system-bot"},
+					{name: "TOS", value: "https://platz1de.github.io/TTHQ/ps-tos"},
+					{name: "Privacy Policy", value: "https://platz1de.github.io/TTHQ/ps-privacy"},
 					{name: "Support", value: "https://discord.gg/TtKJqYwfYe"}
 				).setFooter({text: "Made with ❤️ by the TTHQ Team"}).toJSON()
 		]
