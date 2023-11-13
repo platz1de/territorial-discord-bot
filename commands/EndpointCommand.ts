fee702945f5c8baa2935868729ac219dc0e6fe3f
platz1de <51201131+platz1de@users.noreply.github.com>
2023-11-13 23:04:25 +0100

    Clear a bit of confusion around the endpoint setup

--- commands/EndpointCommand.ts
+++ commands/EndpointCommand.ts
@@ -5,7 +5,7 @@ import {Command, config} from "../PointManager";
 
 export default {
 	slashExclusive: true,
-	slashData: new SlashCommandBuilder().setName("endpoint").setDescription("Setup assistent for the tthq endpoint").setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+	slashData: new SlashCommandBuilder().setName("endpoint").setDescription("Setup assistant for the TTHQ endpoint").setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	execute: async (context: BotUserContext) => {
 		fetch("https://apis.territorial-hq.com/api/Clan/").then(async res => {
 			const data = await res.json();
@@ -13,7 +13,7 @@ export default {
 				for (const clan of data) {
 					if (clan.guildId.toString() === context.guild.id) {
 						if (clan.botEndpoint !== config.endpoint_self + context.guild.id + "/") {
-							await context.reply(createErrorEmbed(context.user, `❌ This server has the wrong endpoint set on the TTHQ api!\nMake sure the \`Custom Bot HttpGet Endpoint\` field is set to\n\`${config.endpoint_self + context.guild.id + "/"}\`\non https://territorial-hq.com/`));
+							await context.reply(createErrorEmbed(context.user, `❌ This server has the wrong endpoint set on the TTHQ api!\nPlease go to https://preview.territorial-hq.com/\nand set the \`Custom Bot HttpGet Endpoint\` field to\n\`${config.endpoint_self + context.guild.id + "/"}\`\nCopy paste the line above as-is`));
 							return;
 						} else {
 							await context.reply(createConfirmationEmbed(context.user, `✅ This server is correctly registered on the TTHQ api!`));
@@ -22,7 +22,7 @@ export default {
 					}
 				}
 			}
-			context.reply(createErrorEmbed(context.user, `❌ This server is not registered on the TTHQ api!\nRegister it on https://territorial-hq.com/ first, or make sure the guild id is correct!`));
+			context.reply(createErrorEmbed(context.user, `❌ This server is not registered on the TTHQ api!\nIf you are registered on https://preview.territorial-hq.com/, make sure the entered guild id is correct!\nOtherwise visit the support server and open a server registration request`));
 		}).catch(async e => {
 			console.error(e);
 			await context.reply(createErrorEmbed(context.user, "❌ An error occurred while requesting the TTHQ api!"));
